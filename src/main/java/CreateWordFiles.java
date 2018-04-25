07d00bdfe4f56f4f1c68d80e51e7bbc431f53b85
Sina Astani <sinaastani@gmail.com>
2018-04-24 21:43:58 -0700

    add word file

---
+++ src/main/java/CreateWordFiles.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class CreateWordFiles{
+    public static void main(String args[]){
+        //get current working directory
+        String dir = System.getProperty("user.dir");
+        try (BufferedReader br = new BufferedReader(new FileReader(dir + File.separator + "words.txt"))){
+            String word_dir_path = dir + File.separator + "files";
+            File word_dir = new File(word_dir_path);
+            word_dir.mkdir();
+            String word = br.readLine();
+            while(word != null){
+                writeCharFile(word, word_dir_path);
+                word = br.readLine();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeCharFile(String s, String fpath){
+        char first_letter = s.charAt(0);
+        first_letter = Character.toUpperCase(first_letter);
+        try {
+            File f = new File(fpath + File.separator + first_letter + ".txt");
+            if(!f.exists()){
+                f.createNewFile();
+            }
+            FileWriter fw = new FileWriter(f.getName(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(s + "\n");
+            bw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/models/Game.java
@@ -0,0 +1,24 @@
+package models;
+
+/**
+ * Created by sinaastani on 4/24/18.
+ */
+public class Game {
+    public static String chooseWord(){
+        String chosen_word = "";
+        String dir = System.getProperty("user.dir");
+        System.out.print(dir);
+
+
+
+
+
+
+
+
+        return chosen_word;
+    }
+    public static void main(String[] args){
+        chooseWord();
+    }
+}
